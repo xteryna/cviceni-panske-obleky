625d432354f0d2b490a5138d88167bfa51513b47
Tereza Beránková <xteryna@seznam.cz>
2024-04-27 16:42:49 +0200

    JS functions improved, new functionality - order button enable/disable and message to customer

--- index.js
+++ index.js
@@ -1,65 +1,82 @@
+
 const colorBtns = document.querySelectorAll('.color-option__box');
-const productImage = document.querySelector('#product-image');
 const orderBtn = document.querySelector('#orderBtn')
-let colorToOrder;
+const productImage = document.querySelector('#product-image');
+const messageElm = document.querySelector('.message p');
 
+const white = {     /**working with english names that need to be transfered to message in Czech language */
+    czech: 'Bílá',
+    availability: 'available'
+}
 
+const red = {
+    czech: 'Červená',
+    availability: 'available'
+}
+
+const black = {
+    czech: 'Černá',
+    availability: 'nA'
+}
+
+const skyblue = {
+    czech: 'Nebesky modrá',
+    availability: 'available'
+}
+
+let colorSelected;
+let order;
+let barva;
 
 
 const selectColor = (selectedButton) => {
-    previouslySelected = document.querySelector('.color-option__box--selected')
-    console.log(selectedButton)
-    let newColor;
-    let shirtColor;
+    const previouslySelected = document.querySelector('.color-option__box--selected');
     let shirtBorderColor = 'black';
 
-    if(selectedButton === document.querySelector('#white')){
-        newColor = 'white';
-    }
-    if(selectedButton === document.querySelector('#black')){
-        newColor = '#141518';
-        shirtBorderColor = 'white';
-    }
-    if(selectedButton === document.querySelector('#red')){
-        newColor = 'red';
-    }
-    if(selectedButton === document.querySelector('#blue')){
-        newColor = 'skyblue';
-    }
-    shirtColor = newColor;
+    selectedButton.classList.toggle('color-option__box--selected');            /**(de)selecting color-button */
 
+    if(previouslySelected === null || selectedButton !== previouslySelected) { /**in case of selecting any color (or changing color) */
+        colorSelected = selectedButton.id;
+        productImage.style.fill = colorSelected;
+        orderBtn.classList.add('order__button--enabled');               /**enabling order-button */
+        orderBtn.removeAttribute('disabled')
 
-    if(previouslySelected === null) {
-        selectedButton.classList.add('color-option__box--selected')
-        productImage.style.fill = newColor;
-        productImage.style.stroke = shirtBorderColor;
-        orderBtn.classList.toggle('order__button--enabled');
-        colorToOrder = newColor;    
-    } else {
-        if (selectedButton !== previouslySelected) {
-            previouslySelected.classList.remove('color-option__box--selected')
-            selectedButton.classList.add('color-option__box--selected')
-            productImage.style.fill = newColor;
-            productImage.style.stroke = shirtBorderColor;
-            colorToOrder = newColor;
-        } else {
-            selectedButton.classList.remove('color-option__box--selected')
-            productImage.style.fill = 'grey';
-            productImage.style.stroke = 'black';
-            orderBtn.classList.toggle('order__button--enabled');
-            colorToOrder = null;
+        if (colorSelected === 'black')  {    /**changing stroke of t-shirt to white to recognize details of black t-shirt */
+            shirtBorderColor = 'white';
         }
+        if (previouslySelected !== null) {                                  /**if changing from one color to another => deselect color-button */
+            previouslySelected.classList.remove('color-option__box--selected')
+        }
+    } else {                                                                /**in case deselecting color => new color will be default-grey */
+        colorSelected = null;                                               /**color back to grey */
+        productImage.style.fill = 'grey';
+        orderBtn.classList.remove('order__button--enabled');                /**disabling order-button */
+        orderBtn.setAttribute('disabled','')
     }
+    productImage.style.stroke = shirtBorderColor;
 
-    return colorToOrder
+    return colorSelected    /**returns either color (order button is then enabled) or null (order button disabled) */
 }
 
+const writeMessage = (color) => {       /**message for customer about availability of selected product */
+    let message
+    if (color.availability === 'available') {
+        message = `${color.czech} košile je na skladě, bude vám dodána v průběhu 3 pracovních dní.`
+    } else {
+        message = `${color.czech} košile bohužel není naskladněna, kontrolujeme stav u dodavatele. O případném naskladnění vás budeme informovat, objednávku momentálně nemůžeme přijmout.`
+    }    
+    messageElm.textContent = message;
+}
 
-colorBtns.forEach( (button) => {
+colorBtns.forEach( (button) => {        /**color button press */        
     button.addEventListener('click', () => {
-        order = selectColor(button);
-        console.log(order)
-        
-
+        colorSelected = selectColor(button);
     })
+})  
+
+orderBtn.addEventListener('click', () => {  /**order button press */  
+    writeMessage(eval(colorSelected));  /**argument - selected color (string) as variable name */ 
 })
+
+
+
