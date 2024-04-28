60bdfd6d6cbe110a62bfb80f677bdfa1c8cf1b74
Marta Bakešová <marta.bakesova@centrum.cz>
2024-04-28 17:08:03 +0200

    zvoleni barvy

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('funguju');
+console.log("funguju");
+
+
+let choosed = document.querySelectorAll(".color-option__box");
+const fce = (event) => {
+  event.target.classList.toggle("border");
+};
+choosed.forEach((vybran) => {
+  vybran.addEventListener("click", fce);
+});
